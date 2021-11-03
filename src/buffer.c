49e262c5daa8387ecc1b41b766411ca6e8c6b1db
Simon Smida <xsmida03@vutbr.cz>
2021-11-03 23:02:37 +0100

    Refactoring source and header files, makefile, adding documentation and some more functionality

--- src/buffer.c
+++ src/buffer.c
@@ -3,22 +3,28 @@
  *  
  * @brief Function implementation for dynamic string manipulation
  * @author Krištof Šiška - xsiska16
+ *         Šimon Šmída   - xsmida03
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "buffer.h"
+#include "include/buffer.h"
 
-STRING_T *init_buffer(){
-    STRING_T *buffer = NULL;
-    buffer = (STRING_T *) malloc(sizeof(STRING_T));
-    if (buffer == NULL){
+
+/**
+ * @brief Initialize buffer - dynamic string manipulation
+ */
+string_t *init_buffer(void)
+{
+    string_t *buffer = NULL;
+    buffer = (string_t *) malloc(sizeof(string_t));
+    if (buffer == NULL) {
         // error
     }
     buffer->string = NULL;
     // Using calloc to avoid getting errors from valgrind
     buffer->string = calloc(BASIC_ALLOC_SIZE, sizeof(char));
-    if (buffer->string == NULL){
+    if (buffer->string == NULL) {
         // error
     }
     buffer->current_index = 0;
@@ -26,10 +32,15 @@ STRING_T *init_buffer(){
     return buffer;
 }
 
-void append_character(STRING_T *buffer, int c){
-     if (buffer->current_index >= buffer->size - 1){
+
+/**
+ * Help procedure to append a character into the buffer
+ */ 
+void append_character(string_t *buffer, int c)
+{
+    if (buffer->current_index >= buffer->size - 1) {
         buffer->string = realloc(buffer->string, buffer->size * 2);
-        if (buffer->string == NULL){
+        if (buffer->string == NULL) {
             fprintf(stderr, "Intern reallo problem in lexer\n");
             exit(1);
         }
@@ -37,12 +48,14 @@ void append_character(STRING_T *buffer, int c){
     } 
     buffer->string[buffer->current_index] = c;
     buffer->current_index = buffer->current_index + 1;
-    return;
 }
 
-void destroy_buffer(STRING_T *buffer){
+
+/**
+ * @brief Deallocate the buffer
+ */
+void destroy_buffer(string_t *buffer)
+{
     free(buffer->string);
     free(buffer);
-    
-    return;
 }
