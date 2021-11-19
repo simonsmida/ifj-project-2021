c46f979f54d496542c8e3ec1787c419e54937aec
Simon Smida <xsmida03@stud.fit.vutbr.cz>
2021-11-19 18:48:40 +0100

    Updating <prog> rule function - including macros

--- src/recursive_descent.c
+++ src/recursive_descent.c
@@ -2,11 +2,17 @@
 #include "include/error.h"
 #include "include/scanner.h"
 
-#define CHECK_RESULT_NOT_OK() if (result != EXIT_OK) return result
+#define CHECK_RESULT_VALUE(_value) \
+    if (result != (_value)) return result 
 
 #define CHECK_TOKEN_ERROR() \
     if (parser->token->type == TOKEN_ERROR) return ERR_LEX
 
+#define GET_TOKEN() \
+        parser->token = get_next_token()
+
+#define CHECK_TOKEN_TYPE(_type) \
+    if (parser->token->type != (_type)) return ERR_SYNTAX;
 
 // Starting nonterminal <prog>
 int prog(parser_t *parser)
@@ -15,30 +21,31 @@ int prog(parser_t *parser)
     
     // <prog> → <prolog> <func_dec> <func_def> <func_call> 'EOF'
     
-    if (parser->token->type == TOKEN_REQUIRE) {
-        // <prolog>
-        result = prolog(parser); 
-        CHECK_RESULT_NOT_OK();
-        // <func_dec>
-        result = func_dec(parser);
-        CHECK_RESULT_NOT_OK();
-        // <func_def>
-        result = func_def(parser);
-        CHECK_RESULT_NOT_OK();
-        // <func_call>
-        result = func_call(parser);
-        CHECK_RESULT_NOT_OK();
-        
-        // Check End Of File
-        if (result != TOKEN_EOF) return ERR_SYNTAX;
-        
-        // TODO: generate instruction for program end
-        //
-        return EXIT_OK;
-    }
+    CHECK_TOKEN_TYPE(TOKEN_REQUIRE);
 
-    // Unexpected token occured - TODO: check error code
-    return ERR_SYNTAX;
+    // <prolog>
+    result = prolog(parser); 
+    CHECK_RESULT_VALUE(EXIT_OK);
+
+    // <func_dec>
+    result = func_dec(parser);
+    CHECK_RESULT_VALUE(EXIT_OK);
+
+    // <func_def>
+    result = func_def(parser);
+    CHECK_RESULT_VALUE(EXIT_OK);
+
+    // <func_call>
+    result = func_call(parser);
+    CHECK_RESULT_VALUE(EXIT_OK);
+    
+    // Check End Of File
+    // TODO: if (result != TOKEN_EOF) return ERR_SYNTAX;
+    CHECK_RESULT_VALUE(TOKEN_EOF);
+    
+    // TODO: generate instruction for program end
+    //
+    return EXIT_OK;
 }
 
 // <prolog>
@@ -65,4 +72,12 @@ int prolog(parser_t *parser)
         return EXIT_OK;
     }
     return ERR_SYNTAX;
+}
+
+// <func_dec>
+int func_dec(parser_t *parser)
+{
+    // <func_dec> → 'global' 'id' ':' 'function' '(' <param_fdec> ')' ':' <ret_type_list> <func_dec>
+
+    if (parser->token->type == TOKEN_GLOBAL)
 }
