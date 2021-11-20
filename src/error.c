b59b2826f4b8cd15b58b341651a7872dd0cb2110
Simon Smida <xsmida03@stud.fit.vutbr.cz>
2021-11-20 12:12:37 +0100

    Adding error code to error_message()

--- src/error.c
+++ src/error.c
@@ -3,14 +3,14 @@
 /**
  *  @brief Write formatted error message to stderr
  */
-void error_message(const char *where, const char *fmt, ...)
+void error_message(const char *where, int err_code, const char *fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
     
     fprintf(stderr, "[%s]: ", where);    
     vfprintf(stderr, fmt, ap);
-    fprintf(stderr, "\n");
+    fprintf(stderr, ", ERROR CODE [%d]\n", err_code);
 
     va_end(ap);
 }
