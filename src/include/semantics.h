5a64baa1282e73648c9f5f3203504c8f7302f48a
Simon Smida <xsmida03@stud.fit.vutbr.cz>
2021-12-06 19:42:02 +0100

    Updating semantics - new semantic checks

--- src/include/semantics.h
+++ src/include/semantics.h
@@ -13,5 +13,17 @@ int check_arg_type_literal(parser_t *parser);
 int check_arg_type_id(parser_t *parser);
 int check_arg_previous_def(parser_t *parser);
 int check_function_redeclaration(parser_t *parser);
+int check_function_call(parser_t *parser);
+int check_function_call_arg_count(parser_t *parser);
+int check_arg_def(parser_t *parser);
+int check_function_redefinition(parser_t *parser);
+int check_param_redeclaration(parser_t *parser);
+int check_param_mismatch(parser_t *parser, int param_index);
+int check_param_count_mismatch(parser_t *parser);
+int check_no_return_values(parser_t *parser);
+int check_return_value_count(parser_t *parser, int ret_type_index);
+int check_return_values(parser_t *parser, int ret_type_index);
+int check_variable_redeclaration(parser_t *parser);
+int check_undeclared_var_or_func(parser_t *parser, symtable_item_t *i);
 
 #endif
